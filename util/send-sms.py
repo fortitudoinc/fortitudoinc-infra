6d8b1401ff0ce9195f2d7c0dcd30cacad9a8c980
Isaac Sears <isaac.j.sears@gmail.com>
2018-09-29 11:40:20 -0400

    Add util script to send sms

---
+++ util/send-sms.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Usage: ./send-sms.py phonenumber msg
+"""
+
+import boto3
+import sys
+
+def usage():
+    print('Usage: {} phonenumber msg'.format(sys.argv[0]))
+    print('\tImportant! Phone number parameter must include country code!')
+
+
+def main():
+    sns = boto3.client('sns')
+
+    # TODO: check length
+    resp = sns.publish(
+        PhoneNumber=sys.argv[1],
+        Message=sys.argv[2],
+        Subject='Fortitudo Telemedicine Message'
+    )
+
+    print(resp)
+
+if __name__ == '__main__':
+    if(len(sys.argv) != 3):
+        usage()
+    else:
+        main()
