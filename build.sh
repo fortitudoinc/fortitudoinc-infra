2fca3d799a96d50757b67bb63101d65fd6605f22
Isaac Sears <isaac.j.sears@gmail.com>
2019-06-22 10:54:13 -0400

    Install modules instead of just packaging them

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ docker image rm $DOCKER_IMAGE_NAME
 for module in $(ls ./external-modules/)
 do
     echo "Building:" $module
-    cd ./external-modules/$module && mvn clean package -DskipTests
+    cd ./external-modules/$module && mvn clean install -DskipTests
     cd -
     cp ./external-modules/$module/omod/target/*.omod ./refapp/omods/
 done
